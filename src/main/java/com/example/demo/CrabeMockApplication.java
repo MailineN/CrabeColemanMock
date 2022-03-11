ad469bea39deb7796449fcd7d16a1a85ebbfbb3d
Mailine Nguyen <mailinenguyen@Flying-Red-Panda.local>
2022-03-11 10:17:20 +0100

    first commit

---
+++ src/main/java/com/example/demo/CrabeMockApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrabeMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrabeMockApplication.class, args);
+	}
+
+}
